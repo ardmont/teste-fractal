f0517e06fe600a0471836e34f702530ef822a2c0
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-26 21:09:39 -0300

    Testes de validação de presença para Album e Music

--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Music, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:duration) }
+  it { should validate_presence_of(:genre) }
 end
