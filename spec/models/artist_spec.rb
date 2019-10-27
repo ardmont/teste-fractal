ff6b2b201a5976cf58d217f7c63e76ba80645b02
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-26 22:36:58 -0300

    Testa a associação de Artist para Album.

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
+  # Teste de associação
+  # Verifica se o model Artist possui uma relação de 1:m com o model Album
+  it { should have_many(:albums) }
+
+  # Testes de validação
+  # Verifica a presensa dos campos name e genre
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:genre) }
 end
