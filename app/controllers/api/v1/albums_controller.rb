8eb64eb1a6c0bda6d9f47a2de2612bd8d11026cd
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-27 17:10:04 -0300

    Associa Album a Artist no controller.

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,51 +1,58 @@
 class Api::V1::AlbumsController < ApplicationController
-  before_action :set_api_v1_album, only: [:show, :update, :destroy]
+  before_action :set_artist, only: [:create, :update]
+  before_action :set_album, only: [:show, :update, :destroy]
 
   # GET /api/v1/albums
   def index
-    @api_v1_albums = Album.all
+    @albums = Album.all
 
-    render json: @api_v1_albums
+    render json: @albums
   end
 
   # GET /api/v1/albums/1
   def show
-    render json: @api_v1_album
+    render json: @album
   end
 
   # POST /api/v1/albums
   def create
-    @api_v1_album = Album.new(api_v1_album_params)
+    # Cria um novo Album e passa o objeto do artist encontrado como valor para o campo artist
+    @album = Album.new(album_params.merge({artist: @artist}))
 
-    if @api_v1_album.save
-      render json: @api_v1_album, status: :created
+    if @album.save
+      render json: @album, status: :created
     else
-      render json: @api_v1_album.errors, status: :unprocessable_entity
+      render json: @album.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/albums/1
   def update
-    if @api_v1_album.update(api_v1_album_params)
-      render json: @api_v1_album
+    if @album.update(album_params.merge({artist: @artist}))
+      render json: @album
     else
-      render json: @api_v1_album.errors, status: :unprocessable_entity
+      render json: @album.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/v1/albums/1
   def destroy
-    @api_v1_album.destroy
+    @album.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_v1_album
-      @api_v1_album = Album.find(params[:id])
+    # Procura e cria a variável do Album com o id informado
+    def set_album
+      @album = Album.find(params[:id])
+    end
+
+    # Procura e cria a variável do Artist com o id informado pelo parâmetro artist_id
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
     end
 
     # Only allow a trusted parameter "white list" through.
-    def api_v1_album_params
-      params.fetch(:album, {}).permit(:title, :genre, :artist)
+    def album_params
+      params.fetch(:album, {}).permit(:title, :genre)
     end
 end
