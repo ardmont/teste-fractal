e94346a952f1f0bc0ee4c60264fe6622c8630930
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-28 10:50:12 -0300

    Cria helper para testes.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,10 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# add custom helpers
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -66,6 +70,9 @@ RSpec.configure do |config|
     # [...]
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
+
+  # add custom helpers
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
