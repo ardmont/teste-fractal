01323cee9791cbe715bdc81f8014a86229fafce2
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-28 11:49:33 -0300

    GET /api/v1/artists/:id

--- spec/requests/api/v1/artists_spec.rb
+++ spec/requests/api/v1/artists_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Api::V1::Artists", type: :request do
   let!(:artists) { create_list(:artist, 10) }
   let(:artist_id) { artists.first.id }
 
+  # Suíte de testes para GET /api/v1/artists
   describe "GET /api/v1/artists" do
     # Faz requisições GET HTTP antes de cada exemplo
     before { get '/api/v1/artists' }
@@ -19,4 +20,29 @@ RSpec.describe "Api::V1::Artists", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  # Suíte de testes para GET /api/v1/artists/:id
+  describe 'GET /api/v1/artists/:id' do
+    before { get "/api/v1/artists/#{artist_id}" }
+
+    context 'Quando existir registro' do
+      it 'returna o artista' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(artist_id)
+      end
+
+      it 'returna status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'Quando não existir registro' do
+      let(:artist_id) { 100 }
+
+      it 'returna status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
 end
