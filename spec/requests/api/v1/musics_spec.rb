9bfc2b9fb909e4bdcfaca9170f670e6f407a7f89
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-28 17:20:58 -0300

    Testa 'GET /api/v1/musics'.

--- spec/requests/api/v1/musics_spec.rb
+++ spec/requests/api/v1/musics_spec.rb
@@ -1,10 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Musics", type: :request do
-  describe "GET /api/v1/musics" do
-    it "works! (now write some real specs)" do
-      get api_v1_musics_path
-      expect(response).to have_http_status(200)
+  # Inicia os dados de teste utilizando a factory criada pelo factory_bot
+  let!(:musics) { create_list(:music, 45) }
+  let(:music_id) { musics.first.id }
+
+  # Suíte de testes para GET /api/v1/musics
+  describe 'GET /api/v1/musics' do
+    context 'GET /api/v1/musics' do
+      # Faz requisições GET HTTP antes de cada exemplo
+      before { get '/api/v1/musics' }
+
+      it 'Retorna lista de músicas sem paginação' do
+        # `json` é um helper que converte o corpo das respostas em json
+        expect(json).not_to be_empty
+        expect(json.size).to eq(30)
+      end
+
+      it 'Retorna status code 200 sem paginação' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'GET /api/v1/musics?page=2' do
+      # Faz requisições GET HTTP antes de cada exemplo
+      before { get '/api/v1/musics?page=2' }
+
+      it 'Retorna lista de músicas com paginação' do
+        # `json` é um helper que converte o corpo das respostas em json
+        expect(json).not_to be_empty
+        expect(json.size).to eq(15)
+      end
+
+      it 'Retorna status code 200 com paginação' do
+        expect(response).to have_http_status(200)
+      end
     end
   end
+
+
 end
