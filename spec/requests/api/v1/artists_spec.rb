f080a0e7e593614f443d1a28372b028fdb13cf4f
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-28 10:50:41 -0300

    Testa GET /api/v1/artists.

--- spec/requests/api/v1/artists_spec.rb
+++ spec/requests/api/v1/artists_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Artists", type: :request do
+  # Inicia os dados de teste utilizando a factory criada pelo factory_bot
+  let!(:artists) { create_list(:artist, 10) }
+  let(:artist_id) { artists.first.id }
+
   describe "GET /api/v1/artists" do
-    it "works! (now write some real specs)" do
-      get api_v1_artists_path
+    # Faz requisições GET HTTP antes de cada exemplo
+    before { get '/api/v1/artists' }
+
+    it 'Retorna lista de artistas' do
+      # `json` é um helper que converte o corpo das respostas em json
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'Retorna status code 200' do
       expect(response).to have_http_status(200)
     end
   end
