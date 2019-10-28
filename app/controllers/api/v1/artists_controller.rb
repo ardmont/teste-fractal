f3669de88cc88988b58fc87b916b062acd71254b
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-28 02:42:14 -0300

    Adiciona albums a resposta de index de artists.

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -4,9 +4,22 @@ class Api::V1::ArtistsController < ApplicationController
   # GET /api/v1/artists
   def index
     # Busca todos os artistas, com paginação de 30 elementos por página e faz eager loading com Albums
-    @artists = Artist.includes(:albums).paginate(page: params[:page], per_page: 30)
+    artists = Artist.includes(:albums).paginate(page: params[:page], per_page: 30)
 
-    render json: @artists
+    # Este array armazenará os artistas e será utilizado como resposta
+    response = []
+
+    artists.each do |artist|
+      # Serializa o objeto artist para posteriormente adicionar os álbums do artista
+      artist_as_json = artist.as_json
+
+      # Adiciona os álbums do artista ao json que será enviado
+      artist_as_json["albums"] = artist.albums
+
+      response << artist_as_json
+    end
+
+    render json: response
   end
 
   # GET /api/v1/artists/:id
