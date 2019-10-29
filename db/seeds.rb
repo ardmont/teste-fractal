e62aef434a27feb7ce0c91e52ecbc7034da333c5
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-28 23:28:18 -0300

    Atualiza o seeds para incluir gêneros.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,38 +2,48 @@ require 'faker'
 
 puts 'Populando banco'
 
-# Cria 50 artistas
-artists = []
-(1..50).each do |i| 
-  artists << { name: Faker::Music.band, genre: Faker::Music.genre }
+# Cria 100 gêneros
+genres = []
+(1..100).each do |i| 
+  genres << { name: Faker::Music.genre }
 end
-puts '(1/3) Inserindo artistas...'
+puts '(1/4) Inserindo gêneros...'
+Genre.create(genres)
+
+# Cria 100 artistas
+artists = []
+(1..100).each do |i| 
+  artists << { name: Faker::Music.band, genre_id: rand(1..100) }
+end
+puts '(2/4) Inserindo artistas...'
 Artist.create(artists)
 
-# Cria 300 músicas
+# Cria 100 músicas
 musics = []
-(1..300).each do |i| 
-  musics << { title: Faker::Lorem.sentence, genre: Faker::Music.genre, duration: Faker::Number.number(digits: 3) }
+(1..100).each do |i| 
+  musics << { title: Faker::Lorem.sentence, genre_id: rand(1..100), duration: Faker::Number.number(digits: 3) }
 end
-puts '(2/3) Inserindo músicas...'
+puts '(3/4) Inserindo músicas...'
 Music.create(musics)
 
-# Cria 180 álbums
+
+# Cria 100 álbums
 albums = []
 puts '(3/3) Inserindo álbums...'
-(1..180).each do |i|
-  album_genre = Faker::Music.genre
+(1..100).each do |i|
+  album_genre = rand(1..100)
   # Busca por um artista aleatoriamente, priorizando o gênero do álbum
-  artist = Artist.where(genre: album_genre).order("RANDOM()").limit(1).first || Artist.order("RANDOM()").limit(1).first
+  artist = Artist.where(genre_id: album_genre).order("RANDOM()").limit(1).first || Artist.order("RANDOM()").limit(1).first
 
-  album = Album.new({ title: Faker::Lorem.sentence, genre: album_genre, artist_id: artist.id })
+  album = Album.new({ title: Faker::Lorem.sentence, genre_id: album_genre, artist_id: artist.id })
 
   # Busca de 1 a 10 músicas aleatoriamente, priorizando o gênero do álbum
-  musics = Music.where(genre: album_genre).order("RANDOM()").limit(rand(1..10)) || Artist.order("RANDOM()").limit(rand(1..10))
+  musics = Music.where(genre_id: album_genre).order("RANDOM()").limit(rand(1..10)) || Artist.order("RANDOM()").limit(rand(1..10))
   album.musics << musics # Associa as músicas ao álbum
 
   # Salva o álbum e as associações no banco
   album.save
 end
+
 
 puts "FIM"
