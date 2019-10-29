decbe684fe16b65b67a3c8fad0eda3f7071d5427
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-10-29 15:53:28 -0300

    Processa as exceptions do apipie.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,6 +3,10 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
+    rescue_from Apipie::ParamError do |e|
+      json_response({ message: e.message }, :unprocessable_entity)
+    end
+  
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
